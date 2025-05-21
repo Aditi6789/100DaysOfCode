fbd0b1f77713c080eba00ca017e44c51c1db713d
Aditi6789 <aditi280604@gmail.com>
2025-05-21 15:57:04 +0530

    Strong Number in java

---
+++ Day2/StrongNumber.java
@@ -0,0 +1,42 @@
+package Day2;
+import java.util.Scanner;
+public class StrongNumber {
+    
+    public static int factorial(int n){
+
+        int factorial=1;
+        while(n != 0){
+            factorial *= n;
+            n--;
+        }
+        return factorial;
+    }
+    public static boolean getIsStrongNumber(int num){
+
+        int temp = num;
+        int sum =0;
+        while(num != 0){
+            int digit = num % 10;
+            sum += factorial(digit);
+            num /= 10;
+        }
+        return temp == sum;
+    }
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        int num = sc.nextInt();
+        sc.close();
+        boolean ans = getIsStrongNumber(num);
+
+        if (ans==true) {
+            System.out.println(num + "  is a Strong Number.");
+        }
+        else{
+            System.out.println(num + "  is not a Strong Number.");
+        }
+
+    }
+
+}
