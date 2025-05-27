a9fd31a5b08edcead7956d6f003fc59eb87b8d7d
Aditi6789 <aditi280604@gmail.com>
2025-05-28 00:48:06 +0530

    Reverse a number in C++  language

---
+++ Day3/ReverseNumber.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int getReverseNumber(int num){
+    // To handle the negative number
+    if(num <0){
+        num = -num;
+        int reversedNum = getReverseNumber(num);
+        reversedNum = -reversedNum;
+        return reversedNum;
+    }
+    // For positive integer number
+    int reversedNum = 0;
+    while(num != 0){
+        reversedNum = (reversedNum* 10) + (num% 10);
+        num = num / 10;
+    }
+    return reversedNum;
+}
+int main(){
+    
+    int originalNum;
+    cout<<"Enter the Number :";
+    cin>>originalNum;
+
+    int reversedNum  =  getReverseNumber(originalNum);
+    cout<<"The reverse number of a given number is : "<<reversedNum;
+}
+
