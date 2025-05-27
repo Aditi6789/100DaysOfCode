0ce12aeed1fa82b8a90ce3ca353044badd861f15
Aditi6789 <aditi280604@gmail.com>
2025-05-27 15:57:35 +0530

    Count the digits in a C++  language

---
+++ Day2/CountDigits.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int countDigits(int num){
+    if(num == 0){
+        return 1;
+    }
+    else{
+        if(num<0){
+            num = -num;
+            return countDigits(num);
+        }
+        else{
+            int count =0;
+            while(num >0){
+                count = count +1;
+                num = num / 10;
+            }
+            return count;
+        }
+    }
+}
+int main(){
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+    cout<<"The number of digit in a number is :"<<countDigits(num);
+}
+
+
